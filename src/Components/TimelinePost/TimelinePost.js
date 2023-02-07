f37517d29f85d2706de6a1be6a03a94bb02c5ecc
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-06 21:00:19 -0700

    adjust some styling for the timeline post

--- src/Components/TimelinePost/TimelinePost.js
+++ src/Components/TimelinePost/TimelinePost.js
@@ -5,9 +5,9 @@ import './_TimelinePost.scss'
 const TimelinePost = () => {
     return (
         <div className="timeline-post-container">
-            <h1>TimelinePost</h1>
-            <RecommendeeCard />
             <RecommendeeIcon />
+            <div className="timeline-circle"></div>
+            <RecommendeeCard />
         </div>
     )
 }
