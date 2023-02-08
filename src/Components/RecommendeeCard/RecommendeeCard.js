2c106bb207a652e9ed5bb67f347512c4c7866f08
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-07 22:33:32 -0700

    improve stlying format for genre categories on recommendee card

--- src/Components/RecommendeeCard/RecommendeeCard.js
+++ src/Components/RecommendeeCard/RecommendeeCard.js
@@ -1,30 +1,37 @@
 import './_RecommendeeCard.scss'
 
 const RecommendeeCard = ({ poster, title, releaseYear, rating, genres, date }) => {
-    date = date.slice(0, 10)
-    date = date.split('-')
-    let day = parseInt(date[2])
-    let month = parseInt(date[1])
-    let year = date[0]
-    switch (month) {
-        case 12: month = "Dec"; break;
-        case 1: month = "Jan"; break;
-        case 2: month = "Feb"; break;
-        case 3: month = "Mar"; break;
-        case 4: month = "Apr"; break;
-        case 5: month = "May"; break;
-        case 6: month = "Jun"; break;
-        case 7: month = "Jul"; break;
-        case 8: month = "Aug"; break;
-        case 9: month = "Sep"; break;
-        case 10: month = "Oct"; break;
-        case 11: month = "Nov"; break;
-    }
-    date = `${month} ${day} ${year}`
+    const allGenres = genres.reduce((genreCategories, currentGenre) => {
+        genreCategories += currentGenre
+        if (currentGenre != genres[genres.length - 1]) {
+            genreCategories += "-"
+        }
+        return genreCategories
+    }, "")
 
-    const allGenres = genres.map(genre => {
-        return genre + " "
-    })
+    const formatDate = () => {
+        date = date.slice(0, 10)
+        date = date.split('-')
+        let day = parseInt(date[2])
+        let month = parseInt(date[1])
+        let year = date[0]
+        switch (month) {
+            case 1: month = "Jan"; break;
+            case 2: month = "Feb"; break;
+            case 3: month = "Mar"; break;
+            case 4: month = "Apr"; break;
+            case 5: month = "May"; break;
+            case 6: month = "Jun"; break;
+            case 7: month = "Jul"; break;
+            case 8: month = "Aug"; break;
+            case 9: month = "Sep"; break;
+            case 10: month = "Oct"; break;
+            case 11: month = "Nov"; break;
+            case 12: month = "Dec"; break;
+        }
+        date = `${month} ${day} ${year}`
+        return date
+    }
 
     return (
         <div>
@@ -36,7 +43,7 @@ const RecommendeeCard = ({ poster, title, releaseYear, rating, genres, date }) =
                     <h3>{allGenres}</h3>
                 </div>
             </div>
-            <h4 className='post-date'>{date}</h4>
+            <h4 className='post-date'>{formatDate()}</h4>
         </div>
     )
 }
