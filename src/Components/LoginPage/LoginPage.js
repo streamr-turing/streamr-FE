232cd949547b3c001bcbc1d540e1e7218fead7a2
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-07 17:40:13 -0700

    Add styling to login page.

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,10 +1,42 @@
 
-import "./_LoginPage.scss"
+import './_LoginPage.scss'
+import tv from '../../tv.png'
 
 const LoginPage = () => {
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
   return (
-    <h1>login page</h1>
+    <div className="login-background">
+      <div className="login-area">
+        <section className="logo-section">
+          <img src={tv} alt='Drawing of a TV' />
+          <h1>Streamr</h1>
+        </section>
+        <form className="username-password">
+        <input
+          type='text'
+          placeholder='Username'
+          name='username'
+          // value={username}
+          // onChange={handleChange}
+          className="input"
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          // value={password}
+          // onChange={handleChange}
+          className="input"
+        />
+        <button onClick={event => handleSubmit(event)}>Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
