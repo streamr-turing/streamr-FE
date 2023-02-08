4b85e7811d44025e167be7c4658a11e5effb14ad
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-08 14:10:08 -0700

    created noRecommendations component with style, setup logic to display feed or noRecommendation message

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { UserContext } from '../../Providers/UserContext'
+import NoRecommendations from '../NoRecommendations/NoRecommendations'
 
 import TimelinePost from "../TimelinePost/TimelinePost"
 import "./_HomePage.scss"
@@ -11,33 +12,41 @@ import "./_HomePage.scss"
 const HomePage = () => {
   const { currentUser } = useContext(UserContext)
   // console.log("HERE: ", currentUser.recommendations[0])
+  
+  const feedResult = () => {
+    if (currentUser.recommendations.length) {
+      const sortedList = currentUser.recommendations.sort((a, b) => {
+        a = a.createdAt.slice(0, 10).split('-').join('')
+        b = b.createdAt.slice(0, 10).split('-').join('')
+        return b - a
+      })
+      const timelinePost = sortedList.map(recommendation => {
+        return (
+          <TimelinePost
+            recommenderAvatar={recommendation.recommender.avatarUrl}
+            recommenderName={recommendation.recommender.username}
+            poster={recommendation.show.thumbnailUrl}
+            title={recommendation.show.title}
+            releaseYear={recommendation.show.releaseYear}
+            rating={recommendation.show.rating}
+            genres={recommendation.show.genres}
+            date={recommendation.createdAt}
+            key={recommendation.id}
+          />
+        )
+      })
+      return timelinePost
+    }
+    else {
+      return <NoRecommendations />
+    }
+  }
 
-  const sortedList = currentUser.recommendations.sort((a, b) => {
-    a = a.createdAt.slice(0, 10).split('-').join('')
-    b = b.createdAt.slice(0, 10).split('-').join('')
-    return b - a
-  })
-
-  const timelinePost = sortedList.map(recommendation => {
-    return (
-      <TimelinePost
-        recommenderAvatar={recommendation.recommender.avatarUrl}
-        recommenderName={recommendation.recommender.username}
-        poster={recommendation.show.thumbnailUrl}
-        title={recommendation.show.title}
-        releaseYear={recommendation.show.releaseYear}
-        rating={recommendation.show.rating}
-        genres={recommendation.show.genres}
-        date={recommendation.createdAt}
-        key={recommendation.id}
-      />
-    )
-  })
 
   return (
     <div className="home-container">
       <h1 className="recommend-title">Recommended By Friends</h1>
-      {timelinePost}
+      {feedResult()}
     </div>
   )
 }
