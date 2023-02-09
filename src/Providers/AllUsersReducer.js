4e8959b9b39178a85b321eea9a8d0446865d60bb
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-08 18:11:27 -0700

    Add AllUsers store and build out functionality for validating username and password and created invalid sign in message.

---
+++ src/Providers/AllUsersReducer.js
@@ -0,0 +1,27 @@
+import AllUsersTypes from './AllUsersTypes'
+
+export const ALL_USERS_INITIAL_STATE = {
+    
+        allUsers:[
+            {
+                "userId": 2,
+                "username": "Hank"
+            },
+            {
+                "userId": 3,
+                "username": "Peggy"
+            },
+            {
+                "userId": 4,
+                "username": "Pappa"
+            }
+                ]
+
+}
+
+const userReducer = (state) => {
+    return state
+}
+
+
+export default userReducer
