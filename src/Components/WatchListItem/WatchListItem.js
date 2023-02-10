e7ee3743b496fd5d772a1eae8ff23f0d166f1f24
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-09 17:38:56 -0700

    added share button styling

--- src/Components/WatchListItem/WatchListItem.js
+++ src/Components/WatchListItem/WatchListItem.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { NavLink, Link } from "react-router-dom"
 
+import shareIcon from "../../images/paper-plane.png"
 import savedTrue from "../../images/bookmark-true.png"
 import savedFalse from "../../images/bookmark-false.png"
 import './_WatchListItem.scss'
@@ -61,10 +62,15 @@ const WatchListItem = ({ poster, title, releaseYear, rating, genres }) => {
             <div className="watch-list-poster-and-info">
                 <img src={poster} className='watch-list-poster-img' />
                 <div className='watch-list-card-info'>
-                    <h1>{title} ({releaseYear})</h1>
+                    <div className="watch-list-title-and-share-container">
+                        <h1>{title} ({releaseYear})</h1>
+                        <div className="share-button">
+                            <img src={shareIcon} className="watch-list-share-icon" />
+                        </div>
+                    </div>
                     <div className="rating-and-genres-container">
                         <h2>Audience Rating: {rating}/10</h2>
-                        <h3>{allGenres}</h3>
+                        <h3 className="watch-list-genres">{allGenres}</h3>
                     </div>
                 </div>
             </div>
