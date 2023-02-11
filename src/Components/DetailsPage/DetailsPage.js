cc0cf854e0c0c520b9e3d4ae9f62c02db23380e1
Sam Rice <samsrice@gmail.com>
2023-02-10 21:07:52 -0800

    update DetailsPage for server data

--- src/Components/DetailsPage/DetailsPage.js
+++ src/Components/DetailsPage/DetailsPage.js
@@ -32,18 +32,21 @@ const DetailsPage = () => {
     GET_SHOW_DETAILS, {
       variables: {
         tmdbId: parseInt(showId),
-        userId: currentUser.id,
+        userId: parseInt(currentUser.id),
         mediaType: "tv"
       }
     }
   )
 
   useEffect(() => {
-    if (data) setWatchlistId(findWatchlistId())
+    if (data) {
+      console.log(data)
+      setWatchlistId(findWatchlistId())
+    }
   }, [data])
 
   const findWatchlistId = () => {
-    const match = currentUser.watchlist.find(show => show.tmdbId === data.tmdbId)
+    const match = currentUser.watchlistItems.find(item => item.show.tmdbId === data.tmdbId)
     return match ? match.watchlistItemId : null
   }
 
@@ -77,9 +80,35 @@ const DetailsPage = () => {
   }
 
   if (loading) return <p>Loading...</p>
-  if (error) navigate("/error") 
+  if (error) {
+    console.log(error)
+    navigate("/error") 
+  }
 
-  const { genres, posterUrl, rating, releaseYear, streamingService, summary, title } = data.showDetails
+  // const { genres, posterUrl, rating, releaseYear, streamingService, summary, title } = data.showDetails
+  const { genres, posterUrl, rating, releaseYear, streamingService, summary, title } = {
+    "tmdbId": 1400,
+    "title": "Seinfeld",
+    "releaseYear": "1989",
+    "streamingService": [
+      {
+        "logoPath": "https://image.tmdb.org/t/p/w500/t2yyOv40HZeVlLjYsCsPHnWLk4W.jpg",
+        "providerName": "Netflix"
+      },
+      {
+        "logoPath": "https://image.tmdb.org/t/p/w500/mShqQVDhHoK7VUbfYG3Un6xE8Mv.jpg",
+        "providerName": "Netflix basic with Ads"
+      }
+    ],
+    "posterUrl": "https://image.tmdb.org/t/p/w500/aCw8ONfyz3AhngVQa1E2Ss4KSUQ.jpg",
+    "genres": [
+      "Comedy"
+    ],
+    "rating": 8.3,
+    "summary": "A stand-up comedian and his three offbeat friends weather the pitfalls and payoffs of life in New York City in the '90s. It's a show about nothing.",
+    "mediaType": "tv",
+    "recommendedBy": []
+  }
 
   return (
     <>
