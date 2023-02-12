6d76c83ae3b906967387ebaf3779227da55d44ce
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-12 10:15:24 -0700

    Add basic code to login_spec and fixutre for same login_spec.

---
+++ cypress/e2e/login_spec.cy.js
@@ -0,0 +1,14 @@
+{/* <reference types="Cypress" />
+
+describe('Testing Login Page', () => {
+
+    beforeEach(() => {
+        cy.intercept('POST',https://streamr-be.herokuapp.com/graphql).as('request')
+        cy.visit('http://localhost:3000/login')
+        cy.wait('@request')
+      })
+
+    it('True should be true', () => {
+        expect('true').to.equal('true')
+    })
+}) */}
