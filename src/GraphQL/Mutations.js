7d4f38d63dd43d321533051c98b44b302fd3ac5b
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-11 18:09:15 -0700

    Add post mutation to add recommendations to the server from the send modal.

--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -25,3 +25,24 @@ export const REMOVE_FROM_WATCHLIST = gql`
     }
   }
 `
+
+export const SEND_RECOMMENDATION = gql`
+  mutation createRecommendation (
+    $tmdbId: Int!
+    $recommenderId: Int!
+    $recommendeeId: Int!
+    $mediaType: String!
+) {
+    createRecommendation (
+        tmdbId: $tmdbId,
+        recommenderId: $recommenderId,
+        recommendeeId: $recommendeeId,
+        mediaType: $mediaType
+    )
+    {
+        tmdbId
+        recommenderId
+        recommendeeId
+    }
+  }
+`
