d0233fff4baee0149439583803a931e31d5cdef9
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-12 16:15:47 -0700

    added recommended by friends logic, design and added scroll feature to long summaries

--- src/Components/DetailsPage/DetailsTable.js
+++ src/Components/DetailsPage/DetailsTable.js
@@ -1,6 +1,9 @@
 import "./_DetailsPage.scss"
 
 const DetailsTable = ({ data }) => {
+
+  console.log('hello papa: ', data.recommendedBy)
+
   const streamingServiceDataResult = () => {
     if (data.streamingService.length) {
       const providerNames = data.streamingService.map(service => {
@@ -16,6 +19,26 @@ const DetailsTable = ({ data }) => {
     }
   }
 
+  const recommendedDataResult = () => {
+    if (data.recommendedBy.length) {
+      const friendNames = data.recommendedBy.map(friend => {
+        return (
+          <div className="detail-recommender-and-avatar-container">
+            <img src={friend.avatarUrl} className='detail-recommender-avatar'/>
+            <p className="detail-recommender-username">{friend.username}</p>
+          </div>
+        )
+      })
+      return friendNames
+    }
+  }
+
+  const recommendedDataTitle = () => {
+    if(data.recommendedBy.length) {
+      return <p className="detail-recommended-by-friends-text">Recommended By Friends:</p>
+    }
+  }
+
   return (
     <div className="detail-mini-data-container">
       <div className="stream-rating-container">
@@ -27,6 +50,10 @@ const DetailsTable = ({ data }) => {
       <p>Audience Rating: {Math.round(data.rating)}/10 ⭐️</p>
       <p className="detail-genre-data">Genre: <b>{data.genres.join(", ")}</b></p>
       <p className="detail-summary-data">{data.summary}</p>
+      <div className="detail-recommend-data">
+        {recommendedDataTitle()}
+        <div className="detail-recommender-data">{recommendedDataResult()}</div>
+      </div>
     </div>
   )
 }
