8846b120364ab618a9ed48e84d397ad83e7b6c20
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-12 12:11:07 -0700

    Create Error Component and base styling.

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,9 +1,15 @@
 import './_Error.scss'
+import { BiError, BiServer } from 'react-icons/bi'
+import fix from '../../images/fixingserver1.png'
 
 const Error = () => {
     return (
-        <>
-        </>
+        <div className="error">
+        <BiError className="error-icon" />
+        <BiServer className="error-icon" />
+        <p >Bummer! The server isn't responding. Our team is working on it! Come back later!</p>
+        <img className="message" src={fix} alt='Legs of a person fixing a server sticking out of wires' />
+        </div>
     )
 }
 
