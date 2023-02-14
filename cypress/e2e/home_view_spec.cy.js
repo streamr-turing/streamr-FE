9827ea6522988148a86785cca98a6a769391d189
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-14 10:47:25 -0700

    WIP: hit bug while trying to verify 2 shows

--- cypress/e2e/home_view_spec.cy.js
+++ cypress/e2e/home_view_spec.cy.js
@@ -14,7 +14,7 @@ describe('Testing Home Page', () => {
         cy.intercept('POST', 'https://streamr-be.herokuapp.com/graphql', (req) => {
             aliasQuery(req, 'fetchUser')
             req.reply({
-                fixture: 'currentUser.json'
+                fixture: 'home-view-currentUser.json'
             })
         })
         cy.get('[type="text"]').type('snoop_dogg')
@@ -23,8 +23,16 @@ describe('Testing Home Page', () => {
         cy.wait('@gqlfetchUserQuery')
     })
 
-    it('Should display main with page name and display timeline feed with recommended shows', () => {
+    it('Should display main with page name', () => {
         cy.get('.recommend-title').should('contain', 'Recommended By Friends')
-        cy.get('.recommender-avatar').eq(0).should('have.attr', 'src', 'https://cdn-icons-png.flaticon.com/512/3940/3940421.png')
     })
+    
+    it('Should display recommenders avatar and name', () => {
+        cy.get('.recommender-avatar').eq(0).should('have.attr', 'src', 'https://cdn-icons-png.flaticon.com/512/3940/3940448.png')
+        cy.get('.recommender-name').eq(0).should('contain', 'martha_stewart')
+    })
+
+    // it('Should display 2 recommended shows', () => {
+    //     cy.get('.recommendee-card-container').eq(0).should('be.visible')
+    // })
 })
