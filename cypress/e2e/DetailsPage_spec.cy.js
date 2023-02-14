f593e82bfa5847b66ba1cbee01b3e723799811d5
Sam Rice <samsrice@gmail.com>
2023-02-14 10:09:22 -0800

    set up DetailsPage spec with all intercepts/fixtures

--- cypress/e2e/DetailsPage_spec.cy.js
+++ cypress/e2e/DetailsPage_spec.cy.js
@@ -1,17 +1,34 @@
-describe("Details Page", () => {
+import { aliasQuery } from "../utilities/graphql-test-utils"
 
+describe("Details Page", () => {
   beforeEach(() => {
-    cy.intercept("POST", "https://streamr-be.herokuapp.com/graphql", req => {
-      // switch (req.body.operationName) {
-      //   case "users":
-      //     req.alias = "users"
-      //     req.reply(res => res.send({ fixture: "users.json" }))
-      //     break
-      // }
+    cy.intercept('POST', 'https://streamr-be.herokuapp.com/graphql', (req) => {
+      switch (req.body.operationName) {
+        case "users":
+          aliasQuery(req, "users")
+          req.reply({ fixture: "login-users.json" })
+          break
+        case "fetchUser":
+          aliasQuery(req, "fetchUser")
+          req.reply({ fixture: "DetailsPage-currentUser.json" })
+          break
+        case "showDetails":
+          aliasQuery(req, "showDetails")
+          req.reply({ fixture: "DetailsPage-showDetails.json" })
+          break
+      }
     })
-  })
-  it("should", () => {
-    
     cy.visit("http://localhost:3000/")
+    cy.wait("@gqlusersQuery")
+    cy.get('[type="text"]').type("snoop_dogg")
+    cy.get('[type="password"]').type("streamr")
+    cy.get("button").click()
+    cy.wait("@gqlfetchUserQuery")
+    cy.get(".home-container a").first().click()
+    cy.wait("@gqlshowDetailsQuery")
+  })
+
+  it("should", () => {
+
   })
 })
