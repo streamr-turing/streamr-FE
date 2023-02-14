993c687f4ce2974b814e94d124b7fe23b8d7638f
Courtney Lippman <courtney.lippman@colorado.edu>
2023-02-13 20:00:09 -0700

    Create initial login testing.

---
+++ src/Utilities/graphql-test-utils.js
@@ -0,0 +1,20 @@
+import { operationName } from "@apollo/client";
+
+export const hasOperationName = (req, operationName) => {
+    const { body } = req
+    return (
+        body.hasOwnProperty('operationName') && body.operationName === operationName
+    )
+}
+
+export const aliasQuery = (req, operationName) => {
+    if (hasOperationName(req, operationName)) {
+      req.alias = `gql${operationName}Query`
+    }
+}
+
+export const aliasMutation = (req, operationName) => {
+    if (hasOperationName(req, operationName)) {
+      req.alias = `gql${operationName}Mutation`
+    }
+  }
