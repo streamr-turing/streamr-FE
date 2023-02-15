3fd019245fb15c1a9e4e428efbd116182905ddbe
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-14 17:43:52 -0700

    Saving progress...restarting cypress

--- cypress/e2e/loading_spec.cy.js
+++ cypress/e2e/loading_spec.cy.js
@@ -24,31 +24,14 @@ describe('Testing Loading Message', () => {
     })
 
     it('Should show "Loading..." message after clicking on show poster to Detail View', () => {
-        // cy.intercept('POST', 'https://streamr-be.herokuapp.com/graphql', (req) => {
-        //     aliasQuery(req, 'showDetails')
-        //     req.reply({
-        //         fixture: 'home-view-showDetails-30Rock.json'
-        //     })
-        // })
-        // cy.get('.poster-img').eq(0).click()
-        // cy.wait('@gqlshowDetailsQuery')
-        // cy.get('.detail-title').should('contain', '30 Rock (2006)')
-
-        it('Should show "Loading..." message after clicking on show poster to Detail View', () => {
-            cy.intercept('POST', 'https://streamr-be.herokuapp.com/graphql', (req) => {
-                aliasQuery(req, 'showDetails')
-                req.reply((res) => {
-                    res.sendDelay = 5000 // Set the response delay to 2 seconds
-                    res.send({
-                        fixture: 'home-view-showDetails-30Rock.json'
-                    })
-                })
+        cy.intercept('POST', 'https://streamr-be.herokuapp.com/graphql', (req) => {
+            aliasQuery(req, 'showDetails')
+            req.reply({
+                fixture: 'home-view-showDetails-30Rock.json'
             })
-
-            cy.get('.poster-img').eq(0).click()
-            cy.wait('@gqlshowDetailsQuery')
-            cy.get('.detail-title').should('contain', '30 Rock (2006)')
         })
-
+        cy.get('.poster-img').eq(0).click()
+        cy.wait('@gqlshowDetailsQuery')
+        cy.get('.detail-title').should('contain', '30 Rock (2006)')
     })
 })
